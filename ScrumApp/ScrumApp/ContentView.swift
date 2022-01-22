d0fc568ba7f1ee402fba37065552e1374087cf28
Lauryn Hudson <laurynhudson@Lauryns-MacBook-Pro.local>
2022-01-22 13:42:53 -0600

    initial commit

---
+++ ScrumApp/ScrumApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ScrumApp
+//
+//  Created by Lauryn Hudson on 1/22/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ ScrumApp/ScrumApp/ScrumAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  ScrumAppApp.swift
+//  ScrumApp
+//
+//  Created by Lauryn Hudson on 1/22/22.
+//
+
+import SwiftUI
+
+@main
+struct ScrumAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
