afd2ddba0a9b04e391413a6ff3c56d74cff6ac80
Lauryn Hudson <laurynhudson@Lauryns-MacBook-Pro.local>
2022-01-22 16:15:10 -0600

    adding Card View

---
+++ ScrumApp/ScrumApp/TrailingIconLabelStyle.swift
@@ -0,0 +1,23 @@
+//
+//  TrailingIconLabelStyle.swift
+//  ScrumApp
+//
+//  Created by Lauryn Hudson on 1/22/22.
+//
+
+import Foundation
+import SwiftUI
+
+struct TrailingIconLabelStyle: LabelStyle {
+    func makeBody(configuration: Configuration) -> some View {
+        HStack {
+            configuration.title
+            configuration.icon
+        }
+    }
+}
+
+
+extension LabelStyle where Self == TrailingIconLabelStyle {
+    static var trailingIcon: Self { Self() }
+}
