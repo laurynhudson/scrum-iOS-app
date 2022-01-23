e5d076b7202922612c6c66a7dcec659272c21438
Lauryn Hudson <laurynhudson@Lauryns-MacBook-Pro.local>
2022-01-22 22:05:15 -0600

    adding scrum list view and setting it to main screen

--- ScrumApp/ScrumApp/ScrumAppApp.swift
+++ ScrumApp/ScrumApp/ScrumAppApp.swift
@@ -11,7 +11,7 @@ import SwiftUI
 struct ScrumAppApp: App {
     var body: some Scene {
         WindowGroup {
-            ContentView()
+            ScrumsView(scrums: DailyScrum.sampleData)
         }
     }
 }
